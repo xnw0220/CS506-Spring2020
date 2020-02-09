140746b0eb85fbe2518c2bc891934098ebb13ec5
Naiwen Xu <xnw1127@bu.edu>
2020-02-08 22:26:23 -0500

    update implementation
    update implementation
    
    Update 02-library/cs506/read.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    modified read.py
    
    modified sim.py
    
    Update 02-library/cs506/sim.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    Update 02-library/cs506/read.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    Update 02-library/cs506/read.py
    
    Co-Authored-By: Lance Galletti <galletti@bu.edu>
    
    Update sim.py
    
    Update read.py
    
    Update read.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,13 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    
+    with open(csv_file_path) as csvDataFile:
+        csvRows = csvDataFile.readlines()
+
+        for row in csvRows:
+            data = row.strip().split(",")
+            matrix.append([eval(val) for val in data])
+            
+    return matrix
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,26 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    return (sum([(a - b) ** 2 for a, b in zip(x, y)]))**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(a-b) for a,b in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    set_x = set(x)
+    set_y = set(y)
+    size_x = len(set_x)
+    size_y = len(set_y)
+    intersect = set_x & set_y
+    size_intersect = len(intersect)
+    jaccard_sim = size_intersect / (size_x + size_y - size_intersect)
+    return 1 - jaccard_sim
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    dot_product_xy = sum(map(lambda z: z[0] * z[1], zip(x, y)))
+    dot_product_xx = sum(map(lambda z: z[0] * z[1], zip(x, x)))
+    dot_product_yy = sum(map(lambda z: z[0] * z[1], zip(y, y)))
+    len_x = (dot_product_xx)**(1/2)
+    len_y = (dot_product_yy)**(1/2)
+    cosine_sim = dot_product_xy / (len_x * len_y)
+    return cosine_sim
 
 # Feel free to add more
